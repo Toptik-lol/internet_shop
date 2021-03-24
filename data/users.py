3217c9c088219d922639d5d5003175b11dfe821a
Toptik_lol <TOPTIKANIME23q>
2021-03-24 21:27:00 +0300

    Модель пользователя для базы данных

---
+++ data/users.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+from db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    phone = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    date_registration = sqlalchemy.Column(sqlalchemy.Date, default=datetime.now)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 
+from data import db_session
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    pass
-    # db_session.global
-
+    db_session.global_init("db/shop.db")
+    app.run()
+    
 
 if __name__ == '__main__':
     main()
