1e5d92fd0ec125156a6b0879c2da2f434c48e307
Toptik_lol <TOPTIKANIME23q>
2021-03-27 19:15:08 +0300

    Сделан обработчик и форма авторизации пользователя

--- main.py
+++ main.py
@@ -35,7 +35,17 @@ def load_user(user_id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('base.html', title='Добавление работы')
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Вход', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
