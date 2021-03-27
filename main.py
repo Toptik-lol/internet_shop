d1eea506869914b856b205cd3020f11c0641028f
Toptik_lol <TOPTIKANIME23q>
2021-03-27 19:08:18 +0300

    Сделан обработчик регистрации пользователя

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.utils import redirect
 
 from data import db_session
 from data.users import User
@@ -19,6 +20,13 @@ def main():
     app.run()
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -33,7 +41,25 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    return render_template('register.html', title='Добавление работы', form=form)
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template("register.html", form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template("register.html", form=form, message="Такой пользователь уже есть")
+
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            phone=form.phone.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
