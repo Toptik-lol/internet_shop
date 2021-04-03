7447f934d5e3960efc7c3b365e112945fb476660
Toptik_lol <TOPTIKANIME23q>
2021-04-03 14:58:28 +0300

    Модель товара для формы

---
+++ forms/product.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, DecimalField, FileField
+from wtforms.validators import DataRequired
+
+
+class ProductForm(FlaskForm):
+    title = StringField('Название товара', validators=[DataRequired()])
+    picture = FileField('Изображение')
+    description = StringField('Описание товара', validators=[DataRequired()])
+    producer = StringField('Производитель', validators=[DataRequired()])
+    price = DecimalField('Цена', validators=[DataRequired()])
+    count = DecimalField('Количество', places=0)
+    advantage = StringField('Преимущества покупки товара')
+    submit = SubmitField('Добавить')
